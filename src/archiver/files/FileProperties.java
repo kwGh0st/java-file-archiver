fc2867d509c3461a4b07ac915f4311c039791159
Karol Programuje <karol341654@gmail.com>
2023-05-27 20:12:11 +0200

    Adding FileProperties class.

---
+++ src/archiver/files/FileProperties.java
@@ -0,0 +1,33 @@
+package archiver.files;
+
+public class FileProperties {
+    private final String name;
+    private final long size;
+    private final long compressedSize;
+    private final int compressionMethod;
+
+    public FileProperties(String name, long size, long compressedSize, int compressionMethod) {
+        this.name = name;
+        this.size = size;
+        this.compressedSize = compressedSize;
+        this.compressionMethod = compressionMethod;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public long getCompressedSize() {
+        return compressedSize;
+    }
+
+    public int getCompressionMethod() {
+        return compressionMethod;
+    }
+
+}
