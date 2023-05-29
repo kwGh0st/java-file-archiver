88a1abf552726e37ab586f901495b6641daba2a6
Karol Programuje <karol341654@gmail.com>
2023-05-29 10:52:31 +0200

    Adding addNewZipEntry method to the ZipFileService c lass.

--- src/archiver/operations/ZipFileService.java
+++ src/archiver/operations/ZipFileService.java
@@ -3,7 +3,10 @@ package archiver.operations;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public abstract class ZipFileService {
     private final Path zipFile;
@@ -22,6 +25,19 @@ public abstract class ZipFileService {
 
         while ((len = in.read()) > 0) {
             out.write(buffer, 0, len);
+        }
+    }
+
+    protected void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws IOException {
+        Path fullPath = filePath.resolve(fileName);
+        try (InputStream inputStream = Files.newInputStream(fullPath)) {
+            ZipEntry entry = new ZipEntry(fileName.toString());
+
+            zipOutputStream.putNextEntry(entry);
+
+            copyData(inputStream, zipOutputStream);
+
+            zipOutputStream.closeEntry();
         }
     }
 }
