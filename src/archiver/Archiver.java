705fdd1c4dcd35442d0ee064962a739214e539d0
Karol Programuje <karol341654@gmail.com>
2023-06-01 16:12:30 +0200

    Implementing ZipRemoveCommand and RemoveZipOperation

--- src/archiver/Archiver.java
+++ src/archiver/Archiver.java
@@ -25,6 +25,7 @@ public class Archiver {
         ConsoleHelper.writeMessage("Select an operation:");
         ConsoleHelper.writeMessage(String.format("\t %d - Zip files into an archive", Operation.CREATE.ordinal()));
         ConsoleHelper.writeMessage(String.format("\t %d - Add file/s into an archive", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - Remove file from the archive.", Operation.REMOVE.ordinal()));
         ConsoleHelper.writeMessage(String.format("\t %d - Extract files from zip archive", Operation.EXTRACT.ordinal()));
         ConsoleHelper.writeMessage(String.format("\t %d - View content of the archive", Operation.CONTENT.ordinal()));
         ConsoleHelper.writeMessage(String.format("\t %d - Exit", Operation.EXIT.ordinal()));
--- src/archiver/command/ZipRemoveCommand.java
+++ src/archiver/command/ZipRemoveCommand.java
@@ -1,8 +1,18 @@
 package archiver.command;
 
+import archiver.ConsoleHelper;
+import archiver.operations.RemoveZipOperation;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipRemoveCommand implements Command {
     @Override
     public void execute() throws Exception {
-
+        ConsoleHelper.writeMessage("Enter the path to the archive.");
+        Path archive = Paths.get(ConsoleHelper.readString());
+        ConsoleHelper.writeMessage("Enter name of the file you want to remove");
+        Path fileName = Paths.get(ConsoleHelper.readString());
+        new RemoveZipOperation(archive).removeFile(fileName);
     }
 }
