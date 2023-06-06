73d345295f2f83d770180d19a86bff6b79404c09
Karol Programuje <karol341654@gmail.com>
2023-06-06 16:26:27 +0200

    Implementing Remove function, adding some necessary helping methods. Completing first GUI. Closes #4 #6

--- src/main/java/archiver/filearchiver/controllers/ButtonController.java
+++ src/main/java/archiver/filearchiver/controllers/ButtonController.java
@@ -2,29 +2,38 @@ package archiver.filearchiver.controllers;
 
 import archiver.filearchiver.exception.NoSuchZipFileException;
 import archiver.filearchiver.exception.PathNotFoundException;
-import archiver.filearchiver.files.FileManager;
 import archiver.filearchiver.files.FileProperties;
-import archiver.filearchiver.model.AddFiles;
-import archiver.filearchiver.model.CreateZip;
-import archiver.filearchiver.model.ExtractZip;
-import archiver.filearchiver.model.ShowContent;
+import archiver.filearchiver.model.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class ButtonController {
+    @FXML
+    private Button select;
+    @FXML
+    private ListView<Path> filesToDelete;
+    @FXML
+    private Button deleteButton;
+
     @FXML
     private Button loadFile;
     @FXML
@@ -37,7 +46,7 @@ public class ButtonController {
     private Button addFiles;
     @FXML
     private ListView<String> fileList;
-
+    private static Path zipSourceToRemove;
 
     @FXML
     void onLoadFileButtonClick(ActionEvent event) throws NoSuchZipFileException, IOException {
@@ -124,9 +133,70 @@ public class ButtonController {
     }
 
     @FXML
-    void onRemoveFilesButtonClick(ActionEvent event) {
+    void onRemoveFilesButtonClick(ActionEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("delete-list.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 480, 380);
+        Stage stage = new Stage();
+        stage.setTitle("Remove Files");
+        stage.setScene(scene);
+        stage.show();
+
     }
 
+    @FXML
+    private void onSelectButtonClick(ActionEvent event) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select source archive.");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Zip File", "*.zip"));
+        Path source = fileChooser.showOpenDialog(select.getContextMenu()).toPath();
+        zipSourceToRemove = source;
 
+        List<Path> pathList = new ArrayList<>();
 
+        try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(source))) {
+            ZipEntry entry = zipInputStream.getNextEntry();
+
+            while (entry != null) {
+                String fileName = entry.getName();
+                pathList.add(Paths.get(fileName));
+                zipInputStream.closeEntry();
+
+                entry = zipInputStream.getNextEntry();
+            }
+        }
+
+        setFilesToDelete(pathList);
+    }
+
+    private void setFilesToDelete(List<Path> fileList) {
+        filesToDelete.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        for (Path path : fileList) {
+            filesToDelete.getItems().add(path);
+        }
+    }
+
+    @FXML
+    private void onDeleteButtonClick(ActionEvent event) throws NoSuchZipFileException, IOException {
+        List<Path> files = filesToDelete.getSelectionModel().getSelectedItems();
+        for (Path path : files) {
+            new RemoveFiles(zipSourceToRemove).removeFile(path);
+        }
+
+        updateList(files, filesToDelete);
+    }
+
+    private void updateList(List<Path> pathList, ListView<Path> view) {
+        List<Path> result = new ArrayList<>();
+        for (Path path : view.getItems()) {
+            if (!pathList.contains(path)) {
+                result.add(path);
+            }
+        }
+        if (view.getItems().size() > 0) view.getItems().remove(0, view.getItems().size());
+
+        for (Path path : result) {
+            view.getItems().add(path);
+        }
+    }
 }
